5c0a4411fcf8a263d4206eeb9e3317fd68865078
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2022-10-12 08:17:56 +0700

    Placeholder commit

--- train.py
+++ train.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     # model
     teacher_ckpt = "sentence-transformers/distiluse-base-multilingual-cased"
     lit_bert_twins = LitBertTwins(
-        teacher_ckpt, student_tokenizer.vocab_size, num_hidden_layers=12,
-        num_attention_heads=12, intermediate_size=3072, pad_token_id=1,
+        teacher_ckpt, student_tokenizer.vocab_size, num_hidden_layers=8,
+        num_attention_heads=8, intermediate_size=1024, pad_token_id=1,
         classifier_dropout=0.2,
     )
 
     # train model
-    trainer = pl.Trainer(max_epochs=30, logger=wandb_logger, devices=2, accelerator="gpu", strategy="ddp")
+    trainer = pl.Trainer(max_epochs=50, logger=wandb_logger, devices=2, accelerator="gpu", strategy="ddp")
     trainer.fit(model=lit_bert_twins, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader)
-    lit_bert_twins.export_model(f"banabert_ot_cl/{wandb_logger.version}")
+    lit_bert_twins.export_model(f"banabert_ot_cl")
