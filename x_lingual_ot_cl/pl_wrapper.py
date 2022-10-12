0b0579a18b7fa7bfd59e01385880aad7f04c50a0
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2022-10-12 12:38:36 +0700

    Update version 0.1.2

--- x_lingual_ot_cl/pl_wrapper.py
+++ x_lingual_ot_cl/pl_wrapper.py
@@ -8,7 +8,7 @@ class LitBertTwins(pl.LightningModule):
     def __init__(
         self, teacher_model: str, vocab_size: int, num_hidden_layers: int,
         num_attention_heads: int, intermediate_size: int, pad_token_id: int,
-        classifier_dropout: float
+        classifier_dropout: float, use_ot: bool=True, use_cl: bool=True,
     ):
         super().__init__()
         self.bert_twins = BertTwins(
@@ -16,9 +16,13 @@ class LitBertTwins(pl.LightningModule):
             num_attention_heads, intermediate_size, pad_token_id,
             classifier_dropout
         )
-        self.ot_loss = SamplesLoss(loss="sinkhorn", p=2, blur=.05)
+        self.use_ot = use_ot
+        self.use_cl = use_cl
         self.main_loss = nn.CrossEntropyLoss()
-        self.consine = nn.CosineSimilarity(dim=1, eps=1e-8)
+        if self.use_ot:
+            self.ot_loss = SamplesLoss(loss="sinkhorn", p=2, blur=.05)
+        if self.use_cl:
+            self.consine = nn.CosineSimilarity(dim=1, eps=1e-8)
         self.save_hyperparameters()
 
     def export_model(self, path):
@@ -34,18 +38,24 @@ class LitBertTwins(pl.LightningModule):
         self.log("train/loss_cls", loss_cls, sync_dist=True)
 
         # OT loss
-        ot_p = self.ot_loss(teacher_output, student_output).mean()
-        self.log("train/ot_p", ot_p, sync_dist=True)
+        if self.use_ot:
+            ot_p = self.ot_loss(teacher_output, student_output).mean()
+            self.log("train/ot_p", ot_p, sync_dist=True)
 
         # contrastive loss
-        s_p = torch.div(self.consine(teacher_output, student_output), 0.05)
-        s_n = torch.div(self.consine(student_output, negative_output), 0.05)
-        cl_loss = (-1) * torch.log(torch.div(torch.exp(s_p), torch.exp(s_p) + torch.exp(s_n))).mean()
+        if self.use_cl:
+            s_p = torch.div(self.consine(teacher_output, student_output), 0.05)
+            s_n = torch.div(self.consine(student_output, negative_output), 0.05)
+            cl_loss = (-1) * torch.log(torch.div(torch.exp(s_p), torch.exp(s_p) + torch.exp(s_n))).mean()
 
-        self.log("train/cl_loss", cl_loss, sync_dist=True)
+            self.log("train/cl_loss", cl_loss, sync_dist=True)
 
         # final loss
-        loss = loss_cls + 0.1*ot_p + 0.5*cl_loss
+        loss = loss_cls
+        if self.use_ot:
+            loss += 0.1*ot_p
+        if self.use_cl:
+            loss += 0.5*cl_loss
         self.log("train/loss", loss, sync_dist=True)
         return loss
 
@@ -59,18 +69,24 @@ class LitBertTwins(pl.LightningModule):
         self.log("valid/loss_cls", loss_cls, sync_dist=True)
 
         # OT loss
-        ot_p = self.ot_loss(teacher_output, student_output).mean()
-        self.log("valid/ot_p", ot_p, sync_dist=True)
+        if self.use_ot:
+            ot_p = self.ot_loss(teacher_output, student_output).mean()
+            self.log("valid/ot_p", ot_p, sync_dist=True)
 
         # contrastive loss
-        s_p = torch.div(self.consine(teacher_output, student_output), 0.05)
-        s_n = torch.div(self.consine(student_output, negative_output), 0.05)
-        cl_loss = (-1) * torch.log(torch.div(torch.exp(s_p), torch.exp(s_p) + torch.exp(s_n))).mean()
+        if self.use_cl:
+            s_p = torch.div(self.consine(teacher_output, student_output), 0.05)
+            s_n = torch.div(self.consine(student_output, negative_output), 0.05)
+            cl_loss = (-1) * torch.log(torch.div(torch.exp(s_p), torch.exp(s_p) + torch.exp(s_n))).mean()
 
-        self.log("valid/cl_loss", cl_loss, sync_dist=True)
+            self.log("valid/cl_loss", cl_loss, sync_dist=True)
 
         # final loss
-        loss = loss_cls + 0.1*ot_p + 0.5*cl_loss
+        loss = loss_cls
+        if self.use_ot:
+            loss += 0.1*ot_p
+        if self.use_cl:
+            loss += 0.5*cl_loss
         self.log("valid/loss", loss, sync_dist=True)
         return loss
 
